2d0b0ca341696199922e801a4907ec2dd3724e9c
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-08-20 23:27:16 +0600

    link deploy problem

--- src/Component/Home/MainHome.jsx
+++ src/Component/Home/MainHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import Projects from "./Projects";
-import OurStories from "./ourStories";
+import OurStories from "./OurStories";
 
 const MainHome = () => {
   return (
